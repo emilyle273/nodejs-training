99027ae30bb9663937d31e341d25da0016edbf46
Nhung Le <nhung.le@asnet.com.vn>
2021-11-06 19:23:17 +0700

    use res.end

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = 3000
 const app = express()
 
 app.get("/:id", (req, res) => {
-  fs.readFile("users2.json", (err, data) => {
+  fs.readFile("users.json","utf8", (err, data) => {
     console.log("err", err)
     if(err) {
       res.status(400).send({message: 'no such file', errorType: 'file_not_found'})
@@ -16,16 +16,17 @@ app.get("/:id", (req, res) => {
       return
     }
 
-    data = JSON.parse(data)
+    // data = JSON.parse(data)
 
-    const id = req.params.id
+    // const id = req.params.id
 
-    let result = data.find(item => item.id === id)
+    // let result = data.find(item => item.id === id)
 
-    if(!result) {
-      res.send("404 - not found")
-    }
-    res.json(result)
+    // if(!result) {
+    //   res.send("404 - not found")
+    // }
+    // res.json(result)
+    res.end(data)
   })
 })
 
